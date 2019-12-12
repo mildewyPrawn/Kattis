06ad0e510b4b6e931acd117f3bc9f4ccc08db86f
Emiliano Galeana <galeanaara@ciencias.unam.mx>
2019-12-12 16:57:57 -0600

    first practice

---
+++ stuck_in_a_time_loop.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define SPELL " Abracadabra"
+
+int main() {
+  int n;
+  cin >> n;
+  int i = 0;
+  while (i++ < n)
+    cout << i << SPELL << endl;
+}
